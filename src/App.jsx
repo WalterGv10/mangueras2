a5f4f914a10269711e4156cca3f99c03c5c03e50
Walter Garcia <wgarcia.10@outlook.com>
2025-03-15 07:56:47 -0600

    Inicializando repositorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import Navbar from './components/navbar/navbar';
-import Bienvenida from './components/bienvenida/bienvenida';
-import Productos from './components/productos/productos';
-import Contacto from './components/contacto/contacto';
-import './App.css';
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/navbar";
+import Bienvenida from "./components/bienvenida/bienvenida";
+import Productos from "./components/productos/productos";
+import Contacto from "./components/contacto/contacto";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "Mangueras y Equipos Hidráulicos, S.A.";
+  }, []);
+
   return (
-    <>
-      <Navbar />
-      <Bienvenida />
-      <Productos />
-      <Contacto />
-    </>
+    <Router>
+      <div className="app-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Bienvenida />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mangueras2/', // 🔹 Asegúrate de poner el nombre de tu repositorio aquí
+  base: '/mangueras2/', // Reemplaza con el nombre de tu repo en GitHub
 });
