67acbec02dfa163939d73ac31f907a19bdfd2ba1
Walter Garcia <wgarcia.10@outlook.com>
2025-03-15 00:08:58 -0600

    Configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/mangueras2/', // 🔹 Asegúrate de poner el nombre de tu repositorio aquí
+});
